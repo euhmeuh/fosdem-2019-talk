16a3375a758f09d2ac1cd9cc3b87cf1ad3be7149
euhmeuh <jerome.martin.dev@gmail.com>
2019-02-02 20:33:09 +0100

    add starting commands in the README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # fosdem-2019-talk
 Lightning talk at FOSDEM 2019 in Bruxelles
+
+## To make the workshop example work:
+
+```
+> cd workshop
+> raco pkg install
+> racket main.rkt # start it
+```
--- workshop/parser.rkt
+++ workshop/parser.rkt
@@ -1,5 +1,5 @@
 #lang brag
-ranch: [/NEWLINE] /RANCH-BEGIN /NEWLINE ponies/ RANCH-END [/NEWLINE]
+ranch: [/NEWLINE] /RANCH-BEGIN /NEWLINE ponies /RANCH-END [/NEWLINE]
 ponies: pony*
 pony: /SPACE* /PONY-BEGIN /SPACE+ name /SPACE+ cry /NEWLINE
 @name: ID
